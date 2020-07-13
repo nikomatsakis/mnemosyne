923153c06374b1daacc1b44875742be7f636a004
Niko Matsakis <niko@alum.mit.edu>
2020-07-13 03:04:56 -0400

    WIP: add messageId

--- MnemosyneLib/types.ts
+++ MnemosyneLib/types.ts
@@ -2,6 +2,7 @@ export interface UserMessage {
     userId: string;
     userName: string;
     chatId: number;
+    messageId: number;
     text: string;
 }
 
@@ -20,6 +21,9 @@ export interface AddWordState {
 
     /// Undefined at first, but eventually set to the text the user gave
     whatWordToAdd?: string;
+
+    /// Message id of the "what word to add" message
+    whatWordToAddMessageId?: number;
 
     /// What language is this word in? (Determined automatically)
     language?: string;
--- TelegramMessageReceived/index.ts
+++ TelegramMessageReceived/index.ts
@@ -3,15 +3,19 @@ import { UserMessage } from "../MnemosyneLib/types";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log(`TelegramMessageReceived(req = ${JSON.stringify(req.body)})`);
-    let userId = req.body.message.from.id.toString();
-    let userName = req.body.message.from.first_name;
-    let chatId = req.body.message.chat.id;
-    let text = req.body.message.text;
-    let message: UserMessage = { userId, userName, chatId, text };
-    context.bindings.userMessages = [message];
-    context.res = {
-        body: "OK"
-    };
+    if (req.body.message !== undefined) {
+        // New message received:
+        let userId = req.body.message.from.id.toString();
+        let userName = req.body.message.from.first_name;
+        let chatId = req.body.message.chat.id;
+        let text = req.body.message.text;
+        let messageId = req.body.message.message_id;
+        let message: UserMessage = { userId, userName, messageId, chatId, text };
+        context.bindings.userMessages = [message];
+        context.res = {
+            body: "OK"
+        };
+    }
 };
 
 export default httpTrigger;
